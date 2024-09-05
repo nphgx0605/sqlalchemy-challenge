2635cdab1819f9191d9f37d23bd603def5034983
Phuong Nguyen <phuongnguyen@Phuongs-MacBook-Pro.local>
2024-09-05 12:59:55 -0400

    SQLAlchemy Challenge Submission

--- Surfup/app.py
+++ Surfup/app.py
@@ -1,13 +1,28 @@
 # Import the dependencies.
+from flask import Flask
+from flask import jsonify 
+from sqlalchemy import create_engine, func
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+import datetime as dt
 
-
-
+app = Flask(__name__)
 #################################################
 # Database Setup
 #################################################
-
+engine = create_engine("sqlite:///Resources/hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect = True)
 
 # reflect an existing database into a new model
+@app.route("/")
+def home():
+    print("Server received request for 'Home' page")
+    return ("Welcome to 'Home' page")
+
+# Save references to each table
+
+
 
 # reflect the tables
 
